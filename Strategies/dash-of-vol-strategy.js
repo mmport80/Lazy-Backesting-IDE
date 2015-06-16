e1589f409e2ad178b43f6be4c76537ccd8fedd84
mmport80 <john.orford@gmail.com>
2015-06-16 11:48:57 +0800

    Lookback changed

--- Strategies/dash-of-vol-strategy.js
+++ Strategies/dash-of-vol-strategy.js
@@ -1,6 +1,6 @@
 var vix = data.INDEX_VIX.daily().lookback(1).prices()[0];
 
-var realisedVol = jStat( data.INDEX_GSPC.daily().lookback(11).returns() ).stdev(true)*100*Math.sqrt(252);
+var realisedVol = jStat( data.INDEX_GSPC.daily().lookback(5).returns() ).stdev(true)*100*Math.sqrt(252);
 
 weight.INDEX_VIX = 1/realisedVol;
 
