05f14db0e1a4c4f0e8f69dc3476a671fb90041e7
John Orford <John.Orford@gmail.com>
2015-06-18 12:32:23 +0800

    test2

--- Strategies/dash-of-vol-strategy.js
+++ Strategies/dash-of-vol-strategy.js
@@ -8,5 +8,3 @@ weight.INDEX_GSPC = 1;
 
 //13 week t-bill yield index used to calculate Sharpe ratio. Scaled for a monthly horizon
 weight.INDEX_IRX = -(weight.INDEX_GSPC+weight.INDEX_VIX)*(4/13);
-
-////
