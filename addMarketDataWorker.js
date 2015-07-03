087b754f893a5263cb783d79f0804640409d6186
John Orford <John.Orford@gmail.com>
2015-07-03 11:14:43 +0800

    files added

---
+++ addMarketDataWorker.js
@@ -0,0 +1,40 @@
+importScripts('lib/lazy.js');
+importScripts('lib/strategyHelpers.js');
+importScripts('lib/dataHelpers.js');
+importScripts('lib/networking2.js');
+importScripts('lib/utility.js');
+
+//*****************************************************
+
+self.addEventListener(
+        'message',
+        function(e) {
+                var d = e.data;   
+                
+                var prices =
+                        //map data from quandl
+                        getQuandlCall(
+                                d.source,
+                                d.ticker,
+                                d.key
+                                )
+                        .xhr()
+                        .map(
+                                function(i){
+                                        var r = null;
+                                        d.y == true ?
+                                                r = {ticker: d.ticker, date: new Date(i[0]), datum:i[1].yieldToDsft()} 
+                                                :
+                                                r = {ticker: d.ticker, date: new Date(i[0]), datum:i[1]};
+                                        return r;
+                                        }
+                                );
+                
+                self.postMessage({ticker:d.ticker,prices:prices,horizon:d.horizon});
+                },
+        false
+        );
+        
+
+//*****************************************************
+
---
+++ formatWorker.js
@@ -0,0 +1,28 @@
+importScripts('lib/lazy.js');
+importScripts('lib/strategyHelpers.js');
+importScripts('lib/dataHelpers.js');
+
+//*****************************************************
+
+self.addEventListener(
+        'message',
+        function(e) {
+                var data = e.data[0];
+                var horizon = e.data[1];
+                
+                
+                
+                //clean
+                cd = data.cleanData();
+                //format
+                fd = cd.formatData(horizon);
+                
+                
+                self.postMessage([cd,fd.toArray()]);
+                },
+        false
+        );
+        
+
+//*****************************************************
+
