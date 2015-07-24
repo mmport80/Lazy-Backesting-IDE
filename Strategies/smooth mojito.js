7f1f5ecef17fff7a89e798dec19878fc0d9e7aa2
mmport80 <john.orford@gmail.com>
2015-07-24 11:35:06 +0800

    smoother mojito - clean up code

--- Strategies/smooth mojito.js
+++ Strategies/smooth mojito.js
@@ -3,28 +3,6 @@ var vix = data.INDEX_VIX.daily().lookback(1).prices()[0];
 
 var ivts = vix/vxv;
 
-
-if(ivts < 0.91){
-  	weight.VXX = -0.7;
-  	weight.NYSEARCA_VXZ = 0.3;
-	}
-else if (ivts < 0.94){
-  	weight.VXX = -0.32;
-  	weight.NYSEARCA_VXZ = 0.68;
-	}
-else if (ivts < 0.97){
-  	weight.VXX = -0.32;
-  	weight.NYSEARCA_VXZ =0.68;
-	}
-else if (ivts < 1.005){
-  	weight.VXX = -0.28;
-  	weight.NYSEARCA_VXZ = 0.72;
-	}
-else {
-  	weight.VXX = 0;
-  	weight.NYSEARCA_VXZ = 1;
-	}
-
 weight.VXX = 11*Math.pow(ivts,2)-17*ivts+5.8;
 weight.NYSEARCA_VXZ = 11*Math.pow(ivts,2)-17*ivts+6.8;
 
