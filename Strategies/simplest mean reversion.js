8190fd7415e7f3a057b5449098ade624a58f79db
John Orford <john.orford@gmail.com>
2016-01-04 20:58:01 +0100

    Delete simplest mean reversion.js

--- Strategies/simplest mean reversion.js
+++
@@ -1,7 +0,0 @@
-
-var f = data.NYSEARCA_FXI.daily().lookback(2).returns()[0];
-weight.NYSEARCA_FXI = -f/Math.abs(f+0.0000000001);
-
-//13 week t-bill yield index used to calculate Sharpe ratio. Scaled for a daily horizon
-weight.INDEX_IRX = -weight.NYSEARCA_FXI/(5*13);
-
